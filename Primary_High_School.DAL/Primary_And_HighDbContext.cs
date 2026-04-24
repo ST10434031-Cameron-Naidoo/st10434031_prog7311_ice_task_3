a406b882320130763d68fd096d13e6bbb4e9320a
ST10434031-Cameron-Naidoo <st10434031@myemeris.edu.za>
2026-04-24 09:36:26 +0200

    Added dbsets to dbcontext file, overrode onModelBuilding method to implement fluent api constraints

--- Primary_High_School.DAL/Primary_And_HighDbContext.cs
+++ Primary_High_School.DAL/Primary_And_HighDbContext.cs
@@ -1,7 +1,35 @@
-﻿namespace Primary_High_School.DAL
-{
-    public class Primary_And_HighDbContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Primary_And_High_School.Domain;
 
+namespace Primary_High_School.DAL
+{
+    public class Primary_And_HighDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Student> Students {get; set;}
+
+        public DbSet<Subject> Subjects { get; set; }
+
+        public Primary_And_HighDbContext(DbContextOptions<Primary_And_HighDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasKey(s =>s.StudentNumber);
+
+            modelBuilder.Entity<Subject>()
+                .HasOne(su=>su.Student)
+                .WithMany(su=>su.Subjects)
+                .HasForeignKey(su=>su.StudentNumber);
+
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Student)
+                .WithMany(b => b.Books)
+                .HasForeignKey(b => b.StudentNumber);
+        }
     }
 }
